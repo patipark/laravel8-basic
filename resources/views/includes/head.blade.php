243e76eacbdc6018fcd7fb4f97fe20e5f6212156
Patipark Sopha <iamwad@gmail.com>
2021-08-13 21:42:33 +0700

    สร้างตัวแปร @yield ขื่อ title เพื่อให้ส่งค่า page title มาจากไฟล์ที่เรียกใช้ layout

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,5 +2,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Page title</title>
+<title>@yield('title')</title>
 <link href="css/style.css" rel="stylesheet">
