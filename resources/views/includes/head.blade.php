0808337538e6f600848a2fc361ab6adc040794eb
Patipark Sopha <iamwad@gmail.com>
2021-08-13 21:38:26 +0700

    เริ่มสร้าง layout จาก โครงสร้าง html5 ก่อน ในที่นี้จะสร้างส่วนหัว จึงตั้งชื่อไฟล์ว่า header.blad.php

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Page title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+    </body>
+</html>
