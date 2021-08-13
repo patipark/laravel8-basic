5cfabbf2f300c7dd9eb427163e075d75d5c74f0e
Patipark Sopha <iamwad@gmail.com>
2021-08-13 22:04:17 +0700

    สร้างไฟล์ resources\views\pages\home.blade.php โดยเรียกใช้งาน layouts.main_template ด้วยคำสั่ง @extends('directory.file') ในที่นี้คือ @extends('layouts.main_template') และกำหนด section ต่างๆด้วยคำสั่ง @sectionพรเช่น title ,contentเพื่อประกอบเป็น p พร้อมกให้ controller เรียกใช้งาน view ไฟล์นี้ด้วยคำสั่ง return view('directory.file');ซึ่งในที่นี้ก็คือ return view('pages.home'); แล้วเปิด browser ดูผ

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(){
-        return 'หน้า home';
+        return view('pages.home');
     }
 
     public function about(){
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main_template')
+@section('title') หน้าหลัก @endsection
+@section('content')
+
+<h1> This is home page</h1>
+
+<div class="container">
+xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+</div>
+
+
+@endsection
