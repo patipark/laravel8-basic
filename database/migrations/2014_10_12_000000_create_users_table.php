35aa369e6607f38cfb30d7aac9cd36f77f6cf534
Patipark Sopha <iamwad@gmail.com>
2021-08-13 22:45:09 +0700

    หลังจาก rollback แล้วลอง แก้ไขไฟล์ database\migrations\2014_10_12_000000_create_users_table.php แก้ไขโครงสร้างใหม่และ แล้วลอง php artisan migrate ผลลัพธ์จะเกิด ตารางใหม่ตามโครงสร้างใหม่. (จะ error หากไม่ได้ rollback)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name',255);
+            $table->string('email',255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',128);
+            $table->text('address');
             $table->rememberToken();
             $table->timestamps();
         });
