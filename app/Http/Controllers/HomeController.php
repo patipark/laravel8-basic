14d9d579c0272f743a93101803f88808989aafa9
Patipark Sopha <iamwad@gmail.com>
2021-08-13 22:11:37 +0700

    สร้างไฟลื ล์ about และเรียกใช้จาก controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     }
 
     public function about(){
-        return 'หน้า about';
+        return view('pages.about');
     }
 
     public function service(){
