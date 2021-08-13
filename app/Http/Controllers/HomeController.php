94fea717595328f364e8e5734d4c17486d8115f9
Patipark Sopha <iamwad@gmail.com>
2021-08-13 22:13:27 +0700

    สสร้างไฟล์ service และเรียก route จาก controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     }
 
     public function service(){
-        return 'หน้า service';
+        return view('pages.service');
     }
 
     public function contact(){
