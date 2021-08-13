f1887e8a570f130204ac95713c11cbd9b2e12813
Patipark Sopha <iamwad@gmail.com>
2021-08-13 21:45:02 +0700

    ใส่ค่า link ของ csด้วยคำสั่ง asset('path to css') โดยจะเรเริ่มอ้างจาก directory public

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,4 +3,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>@yield('title')</title>
-<link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kanit&display=swap">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+<link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+
