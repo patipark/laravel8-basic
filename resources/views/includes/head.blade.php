e5559dffa4039ba168cd8303157a170088d729c6
Patipark Sopha <iamwad@gmail.com>
2021-08-13 21:40:55 +0700

    ตัดให้เหลือเฉพาะ  tag ของ header

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Page title</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
-    </body>
-</html>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Page title</title>
+<link href="css/style.css" rel="stylesheet">
