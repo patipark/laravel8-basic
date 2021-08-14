b2a1d4ad52218186077b1bf0a6b29981e4337c11
Patipark Sopha <iamwad@gmail.com>
2021-08-14 16:48:44 +0700

    การ  insert ข้อมด้วย Model

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -94,9 +94,23 @@ class EmployeeController extends Controller
         // $employees = Employee::where('age', '>=', 27)->get();
 
         // การจัดเรียนข้อมูล และการเลือกข้อมูลบางส่วน
-        $employees = Employee::where('age', '>=', 27)
-            ->orderByDesc('age')
-            ->get();
+        // $employees = Employee::where('age', '>=', 27)
+        //     ->orderByDesc('age')
+        //     ->get();
+
+        // การเพิ่มข้อมูลเข้าไปในตาราง --------------------------------------------
+        $data = array(
+            'fullname' => 'Samit Koyom',
+            'gender' => 'Male',
+            'email' => 'samit@email.com',
+            'tel' => '0898938889389',
+            'age' => 38,
+            'address' => '20/2 moo.2 bangkok',
+            'avartar' => 'noavatar.jpg',
+            'status' => 1
+        );
+
+        $employees = Employee::create($data);
 
         return $employees;
     }
