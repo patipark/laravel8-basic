c838d57b5352db6d11e5c23d2ad3377b6cc3f996
Patipark Sopha <iamwad@gmail.com>
2021-08-14 16:11:15 +0700

    อ่านข้อมูลเพียงรายการเดียวจากตาราง employees

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -10,7 +10,10 @@ class EmployeeController extends Controller
     //
     public function index(){
         // อ่านข้อมูลทั้งหมดจากตาราง employees โดยจะคืนค่าเป็น json data
-        $employees = DB::table('employees')->get(); // select * from employees
+        // $employees = DB::table('employees')->get(); // select * from employees
+
+        // อ่านข้อมูลเพียงรายการเดียวจากตาราง employees
+        $employees = DB::table('employees')->first(); // select * from employees limit 1
 
         return $employees;
     }
