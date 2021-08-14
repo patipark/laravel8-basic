00385d3eecba4f1fafab04ad51ebff02c07719fb
Patipark Sopha <iamwad@gmail.com>
2021-08-14 16:12:21 +0700

    อ่านข้อมูลแบบระบุ field ที่ต้องการ

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -13,7 +13,10 @@ class EmployeeController extends Controller
         // $employees = DB::table('employees')->get(); // select * from employees
 
         // อ่านข้อมูลเพียงรายการเดียวจากตาราง employees
-        $employees = DB::table('employees')->first(); // select * from employees limit 1
+        // $employees = DB::table('employees')->first(); // select * from employees limit 1
+
+        // อ่านข้อมูลแบบระบุ field ที่ต้องการ
+        $employees = DB::table('employees')->first(['fullname','gender','email']); // select fullname, gender, email from employees
 
         return $employees;
     }
